ae84afad8a3f3d69020a947bf8ec22ebe624399a
github username <github email address>
2017-05-08 18:40:21 -0400

    built some views

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,29 @@
 class PicturesController < ApplicationController
+  def new
+    @picture = Picture.new
+
+  end
+
+  def create
+    # byebug
+    @picture = Picture.create(picture_params[:id])
+    redirect_to new_picture_path(@picture) unless @picture.save
+  end
+
+  def show
+    byebug
+    @picture = Picture.find(params[:id])
+  end
+
+  def destroy
+    @picture.delete
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:image_url, :title, :user_id)
+
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+
+  end
+
+  def create
+    # byebug
+    @user = User.create(user_params[:id])
+    redirect_to user_path(@user) unless @user.save
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user.delete
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
+
+  end
+
 end
