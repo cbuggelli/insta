b38bf80921fc05f8fab516d206a7286b8e36afbb
github username <github email address>
2017-05-09 15:07:41 -0400

    picture views

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,12 +1,17 @@
 class PicturesController < ApplicationController
+
+  def index
+    @pictures = Picture.all
+  end
+
   def new
     @picture = Picture.new
-
   end
 
   def create
-    # byebug
+
     @picture = Picture.create(picture_params[:id])
+    byebug
     redirect_to new_picture_path(@picture) unless @picture.save
   end
 
@@ -21,8 +26,8 @@ class PicturesController < ApplicationController
 
   private
 
-  def picture_params
-    params.require(:picture).permit(:image_url, :title, :user_id)
+  def picture_params (*args)
+    params.require(:picture).permit(*args)
 
   end
 
