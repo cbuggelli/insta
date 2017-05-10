ab0eeb0212060c3587868f61dd5f81a4c08b0f6f
github username <github email address>
2017-05-10 09:35:10 -0400

    added file extension validation

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,4 +3,7 @@ class Picture < ApplicationRecord
   has_many :comments
   has_many :picture_tags
   has_many :tags, through: :picture_tags
+
+  validates :image_url, allow_blank: false, format: { with: %r{.(gif|jpg|jpeg|png|bmp)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.' }
+
 end
