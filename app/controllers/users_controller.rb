3e873a228b46add593b5a93dc349ca63e5af20ce
github username <github email address>
2017-05-11 11:02:26 -0400

    fixed password confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,22 +6,23 @@ class UsersController < ApplicationController
   end
 
   def create
-    if (user_params[:password] == user_params[:password_confirmation])
-      @user = User.new(user_params)
-      # byebug
+    if (user_params[:password] != user_params[:password_confirmation])
+      flash[:notice] = "Password does not match confirmation, please try again"
     end
+
+    @user = User.new(user_params)
+    
     if @user.valid?
       @user.save
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      flash[:notice] = "Invalid credentials!, please try again"
+      flash[:notice] = "Invalid credentials, please try again"
       redirect_to new_user_path
     end
   end
 
   def show
-    # byebug
     @user = User.find(params[:id])
     @pictures = @user.pictures
   end
@@ -33,7 +34,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username,:password,:email)
+    params.require(:user).permit(:username, :password, :password_confirmation, :email)
   end
 
 end
