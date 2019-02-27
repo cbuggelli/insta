6660d45809cd10762581e4c87fb7a566a5048ad6
github username <cjab86@gmail.com>
2019-02-27 11:33:02 -0500

    wrote test for followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,20 @@ class UsersController < ApplicationController
     @user.delete
   end
 
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   private
 
   def user_params
